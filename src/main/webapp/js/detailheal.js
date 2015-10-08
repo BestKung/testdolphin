2f620214cbe25091f098cea03a14f514d6b9ab70
metavin007 <meta.vin@hotmail.com>
2015-10-08 12:10:26 +0700

    addO2

--- src/main/webapp/js/detailheal.js
+++ src/main/webapp/js/detailheal.js
@@ -1,17 +1,29 @@
 angular.module('detailHeal', []);
 angular.module('detailHeal').controller('detailHealController', function ($scope, $http) {
-    
+
     $scope.orderHeals = {};
-    
+
     loadOrderHeal();
-    function loadOrderHeal(){
-        $http.get('loadorderheal').success(function (data){
-          $scope.orderHeals = data;
-          console.log($scope.orderHeals);
-        }).error(function (data){
-            
+    function loadOrderHeal() {
+        $http.get('loadorderheal').success(function (data) {
+            $scope.orderHeals = data;
+            console.log($scope.orderHeals);
+        }).error(function (data) {
+
         });
     }
-    
+
+
+    $scope.actionSelectHeal = function () {
+        $('#modalSelectHeal').openModal({dismissible: false});
+    };
+
+    $('.datepicker').pickadate({
+        selectMonths: true,
+        selectYears: 200,
+        format: 'yyyy-mm-dd',
+        container: 'body'
+    });
+
 });
 
